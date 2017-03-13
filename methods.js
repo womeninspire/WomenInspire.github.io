c47a0820c2af8da3e44f3e677391b8fd8ab64ec3
womeninspire <alice.perepyolkina@mcsaatchi.com>
2017-03-13 18:34:05 +0000

    committed changes

--- methods.js
+++ methods.js
@@ -1,7 +1,18 @@
-$(function () {
-  $("#contact-us").click(function() {
-      $('html,body').animate({
-          scrollTop: $("#get-in-touch").offset().top},
-          'slow');
+function getInTouchScroll() {
+    $("html,body").animate({
+        scrollTop: $("#get-in-touch").offset().top
+      },
+      "slow");
+  }
+
+  $(document).ready(function(){
+    if(window.location.href.indexOf("contact-us") > -1){
+        getInTouchScroll();
+    }
   });
-});
+
+  $(function () {
+   $("#contact-us").click(function() {
+    getInTouchScroll();
+   });
+ });
